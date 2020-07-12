6659f7d4814c463ef9385c8db60152c7c3306e95
Caroline <carolinegardeli@hotmail.com>
2020-07-12 01:52:58 -0300

    Update e_commerce/urls.py & modify some products images

--- src/e_commerce/urls.py
+++ src/e_commerce/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from ..products.views import ProductListView, ProductDetailView, ProductFeaturedListView, ProductFeaturedDetailView
+from products.views import ProductListView, ProductDetailView, ProductFeaturedListView, ProductFeaturedDetailView
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
