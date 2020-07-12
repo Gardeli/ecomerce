dea96eb4e351a01e29bd7b79e3bf45388557eb06
Caroline <carolinegardeli@hotmail.com>
2020-07-12 00:28:25 -0300

    Create ProductonManager in products/models.py & update products/views.py

--- src/products/models.py
+++ src/products/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
 
 # product_category
 class Product(models.Model):
@@ -7,6 +13,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
--- src/products/views.py
+++ src/products/views.py
@@ -1,7 +1,10 @@
+from django.http import Http404
+
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product
+
 
 class ProductListView(ListView):
     # traz todos os produtos do banco de dados sem filtrar nada
@@ -16,10 +19,17 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     # traz todos os produtos do banco de dados sem filtrar nada
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
+
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
