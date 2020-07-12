287c62601290eb32c2f689ec4e0917d91f3696ed
Caroline <carolinegardeli@hotmail.com>
2020-07-11 23:10:04 -0300

    Update products/views.py|create products/templates/products/detail.html

--- src/products/views.py
+++ src/products/views.py
@@ -1,18 +1,17 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
-
 class ProductListView(ListView):
-    # lista todos os produtos do banco de dados (sem filtro)
+    # traz todos os produtos do banco de dados sem filtrar nada
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
-    #def get_context_data(self, *args, **kwargs):
-    #    context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #    print(context)
-    #    return context
+    # def get_context_data(self):
+    # context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+    # print(context)
+    # return context
 
 
 class ProductDetailView(DetailView):
@@ -20,7 +19,7 @@ class ProductDetailView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
 
-    # def get_context_data(self, *args, **kwargs):
-    # context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-    # print(context)
-    # return context
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
