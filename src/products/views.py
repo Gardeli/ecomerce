13eaf37827c0ce331e349441f27ab55afe4eedfc
Caroline <carolinegardeli@hotmail.com>
2020-07-12 02:29:23 -0300

    Configure slug view class in products/views.py
    Configure MultipleObjectReturned except in get_object method in ProductDetailSlugView class

--- src/products/views.py
+++ src/products/views.py
@@ -29,6 +29,23 @@ class ProductListView(ListView):
     # return context
 
 
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        slug = self.kwargs.get('slug')
+        #instance = get_object_or_404(Product, slug = slug, active = True)
+        try:
+            instance = Product.objects.get(slug = slug, active = True)
+        except Product.DoesNotExist:
+            raise Http404("Não encontrado!")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug = slug, active = True)
+            instance =  qs.first()
+        return instance
+
+
 class ProductDetailView(DetailView):
     # traz todos os produtos do banco de dados sem filtrar nada
     # queryset = Product.objects.all()
