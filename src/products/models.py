64f968cde9991e1f8eb417c2d40ac15829bf44e4
Caroline <carolinegardeli@hotmail.com>
2020-07-12 02:33:40 -0300

    Set unique=True in slug field parameter in products/models.py

--- src/products/models.py
+++ src/products/models.py
@@ -31,7 +31,7 @@ class ProductManager(models.Manager):
 # product_category
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
