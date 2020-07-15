65a25cfa457a339dcc14da6a0385e667e63f955c
Caroline <carolinegardeli@hotmail.com>
2020-07-15 16:00:19 -0300

    Modify search/urls.py & search/views.py

--- src/search/views.py
+++ src/search/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from products.models import Product
 
-# Create your views here.
+
+class SearchProductView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kargs):
+        request = self.request
+        result = request.GET
+        query = result.get('q', None)
+        if query is not None:
+            return Product.objects.filter(title__icontains=query)
+        return Product.objects.none()
