4932edaf49f1d4c80b6c97ce9d74ae4c26caa741
Caroline <carolinegardeli@hotmail.com>
2020-07-19 23:57:21 -0300

    Display user query in search bar

--- src/search/urls.py
+++ src/search/urls.py
@@ -5,5 +5,5 @@ app_name = "search"
 from .views import SearchProductView
 
 urlpatterns = [
-    path('', SearchProductView.as_view(), name='list'),
+    path('', SearchProductView.as_view(), name='query'),
 ]
